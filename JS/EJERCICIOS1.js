adee1b42bd664d121a592a60ae0709a251c12527
Natalia Figueroa <nataliaafigueroa@gmail.com>
2022-06-17 21:45:19 -0300

    hola

---
+++ JS/EJERCICIOS1.js
@@ -0,0 +1,66 @@
+// realizar un programa que imprima 25 términos de la serie 11 22 33 44 55 66 77 (no se ingresan valores por teclado)
+
+// function mostrarTerminos(){
+
+//     let terminos = [];
+//     for(let i=0;i<26; i++){
+//         terminos.push(11*i)
+// }return(terminos)
+// }
+// console.log(mostrarTerminos())
+
+
+//EJERCICIO 2 // MOSTRAR LOS MÚLTIPLOS DE 8 HASTA EL VALOR 500. DEBE APARECER EN PANTALLA 8 / 16 / 24 / ETC. 
+
+// function mostrarResultado(){
+//     let multiplos = [];
+//     for(let i=0;i*8<500; i++){
+//         multiplos.push(i*8)
+//     }
+//     return(multiplos)
+// }
+// console.log(mostrarResultado())
+
+//EJERCICIO 3 // ESCRIBIR UN PROGRAMA QUE LEA 10 NOTAS DE ALUMNOS Y NOS INFORME CUANTOS TIENEN NOTAS MAYORES O IGUALES A 7 O MENORES
+
+// function mostrarNotas(){
+
+//     let notas = [1,2,3,4,5,6,7,8,9,10];
+//     let mayor = 0
+//     let menor = 0
+
+//         for(let i=0;i<notas.length; i++){
+    
+//             if(notas[i]>=7){
+//             mayor++
+//             }else{
+//             menor++
+//             }
+//         }
+//         console.log(mayor,menor)
+// //        let objeto= {mayor_7:mayor,menor_7:menor}
+// //         return objeto["mayor_7"]
+// }
+// mostrarNotas()
+
+//EJERCICIO 4 // 
+
+// SE INGRESAN UN CONJUNTO DE 5 ALTURAS DE PERSONAS POR TECLADO. MOSTRAR LA ALTURA PROMEDIO DE LAS PERSONAS. 
+
+function mostrarAlturas(){
+
+    let alturas = [];
+    let altura = 0
+
+        for(let i=0;i<5; i++){
+            alturas = prompt("Ingresar numero")
+            
+        }
+        console.log(altura)
+}
+mostrarAlturas()
+
+
+   
+    
+
